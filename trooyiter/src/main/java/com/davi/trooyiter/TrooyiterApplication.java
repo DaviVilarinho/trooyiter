144627e3d35355eae75c153bcfbbe9a81ebcd11a
vilarinux <davi.r.vilarinho@gmail.com>
2021-07-19 22:38:42 -0300

    spring initializr

---
+++ trooyiter/src/main/java/com/davi/trooyiter/TrooyiterApplication.java
@@ -0,0 +1,13 @@
+package com.davi.trooyiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrooyiterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrooyiterApplication.class, args);
+	}
+
+}
---
+++ trooyiter/src/test/java/com/davi/trooyiter/TrooyiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davi.trooyiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrooyiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
